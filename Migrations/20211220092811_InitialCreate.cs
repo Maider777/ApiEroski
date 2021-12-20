08311179236f3d6f80d46d3dbee4757e5f3d308e
Maider777 <maidertxu13@gmail.com>
2021-12-20 11:46:17 +0100

    primer commit

---
+++ Migrations/20211220092811_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiEroski.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TicketItem",
+                columns: table => new
+                {
+                    Nombre = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    NumTicket = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketItem", x => x.Nombre);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketItem");
+        }
+    }
+}
---
+++ Migrations/20211220094603_Initi.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiEroski.Migrations
+{
+    public partial class Initi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TicketItem",
+                table: "TicketItem");
+
+            migrationBuilder.RenameTable(
+                name: "TicketItem",
+                newName: "TicketsItem");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TicketsItem",
+                table: "TicketsItem",
+                column: "Nombre");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TicketsItem",
+                table: "TicketsItem");
+
+            migrationBuilder.RenameTable(
+                name: "TicketsItem",
+                newName: "TicketItem");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TicketItem",
+                table: "TicketItem",
+                column: "Nombre");
+        }
+    }
+}
